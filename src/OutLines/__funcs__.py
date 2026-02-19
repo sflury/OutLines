20f017cf142daba2e8d33a7daaad94a27b52adcf
Sophia Flury <42982705+sflury@users.noreply.github.com>
2026-02-19 16:52:40 +0000

    patch to geometry funcs
    Fixed bug in cone geometry functions where arc length dl was undefined. Added some additional comments/documentation.

--- src/OutLines/__funcs__.py
+++ src/OutLines/__funcs__.py
@@ -76,11 +76,11 @@ def dens_norm(u,beta,vini,VF,xi,sigma):
 # ensemble with linear spacing
 def dens_pulseslin(u,beta,vini,VF,sigma,dx,x0,pulse='Normal'):
     dens_pulse = n[pulse]
-    return sum([dens_pulse(u,beta,vini,VF,x,sigma) for x in arange(x0,x[VF](0.95,beta,vini)+dx,dx)],axis=0)
+    return sum([dens_pulse(u,beta,vini,VF,x,sigma) for x in arange(x0,x[VF](0.99,beta,vini)+dx,dx)],axis=0)
 # ensemble with log spacing
 def dens_pulseslog(u,beta,vini,VF,sigma,dx,x0,pulse='Normal'):
     dens_pulse = n[pulse]
-    return sum([dens_pulse(u,beta,vini,VF,x,sigma) for x in 10**arange(log10(x0),log10(x[VF](0.95,beta,vini))+dx,dx)],axis=0)
+    return sum([dens_pulse(u,beta,vini,VF,x,sigma) for x in 10**arange(log10(x0),log10(x[VF](0.99,beta,vini))+dx,dx)],axis=0)
 # damped ensemble with linear spacing
 def dens_pulsedamplin(u,beta,vini,VF,r,sigma,dx,x0,pulse='Normal'):
     return dens_pulseslin(u,beta,vini,VF,sigma,dx,x0,pulse=pulse) * dens_exp(u,beta,vini,VF,r)
@@ -322,12 +322,15 @@ def cone_inscr(u,vinf,S,g,h,A,B,C,i,t,rdisk,rdisk_sini,adisk,cone,w):
     # if w cannot project onto u
     # then return no circle
     if u > w : return 0.
-    # radius of deprojected velocity from cos varrho = u/w
+    # radius sin varrho of deprojected velocity
+    # from cos varrho = u/w and sin^2 + cos^2 = 1
     rdprv = sqrt(1-(u/w)**2)
     # intersection points (p,q) of deprojected velocity with projected spherical cap
     C += rdprv**2
     qc = (B+array([-1,1])*sqrt(B**2-4*A*C))/(2*A)
     # pc = sqrt(rbnd**2-qc**2) # --> no need to calculate
+    # unit arc length
+    dl = 0.
     # if no intersection occurs, qc[0] (always smaller than qc[1]) > rdprv
     # or b^2 < 4ac, giving a NaN intersection
     # if u/w close to 1, then arc length -> 0 due to integral limit
@@ -337,7 +340,6 @@ def cone_inscr(u,vinf,S,g,h,A,B,C,i,t,rdisk,rdisk_sini,adisk,cone,w):
     elif rdprv < h-S : return 1.
     # otherwise, full cone treatment
     else:
-        dl = 0.
         # if first intersection below x-axis,
         # include full deprojected velocity minus the angle subtended by (pc,qc)
         if qc[0] < 0 and qc[0] > -rdprv : dl += 1-arccos(qc[0]/rdprv)/pi
@@ -368,6 +370,8 @@ def cndk_inscr(u,vinf,S,g,h,A,B,C,i,t,rdisk,rdisk_sini,adisk,cone,w):
     # intersection points (pd,qd) of deprojected velocity with projected disk
     qd = sqrt((rdprv**2-rdisk**2)/adisk)
     # pc = sqrt(rbnd**2-qc**2) # --> no need to calculate
+    # unit arc length
+    dl = 0.
     # if no intersection occurs, qc[0] (always smaller than qc[1]) > rdprv
     # or b^2 < 4ac, giving a NaN intersection
     # if u/w close to 1, then arc length -> 0 due to integral limit
@@ -390,7 +394,6 @@ def cndk_inscr(u,vinf,S,g,h,A,B,C,i,t,rdisk,rdisk_sini,adisk,cone,w):
     elif rdisk > rdprv and rdisk > 2**-20 :
         # the cone is posterior to disk
         if cone == 'post' :
-            dl = 0.
             # disk is below projected cone -- no obstruction
             if qc[0] > 0 and qc[0] > qd : dl += arccos(qc[0]/rdprv)/pi
             # disk obstructs lower part of projected cone
@@ -401,7 +404,6 @@ def cndk_inscr(u,vinf,S,g,h,A,B,C,i,t,rdisk,rdisk_sini,adisk,cone,w):
             if qc[1] <= rdprv and i+t > pi/2 : dl += arccos(qc[1]/rdprv)/pi
         # if cone is anterior to disk
         elif cone == 'ante' :
-            dl = 0.
             # if first intersection below x-axis,
             # include full deprojected velocity minus the angle subtended by (pc,qc)
             if qc[0] < 0 and qc[0] > -rdprv : dl += 1-arccos(qc[0]/rdprv)/pi
@@ -419,7 +421,6 @@ def cndk_inscr(u,vinf,S,g,h,A,B,C,i,t,rdisk,rdisk_sini,adisk,cone,w):
                 elif qc[1] < qd and qd < rdprv : dl += arccos(qd/rdprv)/pi
     # if no disk or if disk radius < deprojected velocity radius, just consider the cones
     else:
-        dl = 0.
         # if first intersection below x-axis,
         # include full deprojected velocity minus the angle subtended by (pc,qc)
         if qc[0] < 0 and qc[0] > -rdprv : dl += 1-arccos(qc[0]/rdprv)/pi
